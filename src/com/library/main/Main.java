83ba11fe28786da99c5c2882a5e58711f5ff1aaa
hande <hande.dann@gmail.com>
2025-03-28 13:13:00 +0300

    packages

---
+++ src/com/library/main/Main.java
@@ -0,0 +1,15 @@
+package com.library.main;
+
+public class Main {
+    public static void main(String[] args) {
+        // package yapısı:
+        // com.library
+        // models # Entity sınıfları
+        // services # iş mantığı (kitap ödünç alma, iade vb.)
+        // repository # veri saklama işlemleri
+        // utils # yardımcı methodlar
+        // main # ana çalışma dosyaları
+
+
+    }
+}
